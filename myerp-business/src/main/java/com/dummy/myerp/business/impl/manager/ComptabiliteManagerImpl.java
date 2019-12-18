861bd75fb870e5e8560f071f11aa2df340e727e9
Fabien Joseph <styleure@ascentia.fr>
2019-12-18 17:12:10 +0100

    test soutenance

--- myerp-business/src/main/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImpl.java
+++ myerp-business/src/main/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImpl.java
@@ -30,7 +30,6 @@ public class ComptabiliteManagerImpl extends AbstractBusinessManager implements 
 
     // ==================== Attributs ====================
 
-
     // ==================== Constructeurs ====================
 
     /**
